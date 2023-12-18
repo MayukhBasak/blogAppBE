e346accabae6a8947f11d51314bef95bea7f0b86
MayukhBasak <106827187+MayukhBasak@users.noreply.github.com>
2023-12-19 00:41:05 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const database = "mongodb+srv://mayukhbasak2003:KJGpEhxOAVq1UUVv@cluster0.dkhksad.mongodb.net/blogDatabase";
+const database = "mongodb+srv://mayukhbasak2003:******************@cluster0.dkhksad.mongodb.net/blogDatabase";
 
 const dbConnect = () => {
   mongoose
